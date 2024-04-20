63d580c957f6561f558f4ddccbc296ff5a758f49
adama diouf <adamadiouf2017@gmail.com>
2024-04-20 03:57:55 -0500

    add new thing

--- app/_utils/GlobalApi.jsx
+++ app/_utils/GlobalApi.jsx
@@ -2,10 +2,10 @@ const { default: axios } = require("axios")
 
 const API_KEY = process.env.NEXT_PUBLIC_STRAPI_API_KEY
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:1337/api',
-  headers:{
-   "Authorization":`Bearer ${API_KEY} ` 
-  }
+  baseURL: 'https://absacare-backend.onrender.com/api',
+  headers: {
+    Authorization: `Bearer ${API_KEY} `,
+  },
 })
 const getCategory =()=> axiosClient.get(`/categories?populate=*`)
 const getDoctorList = () => axiosClient.get(`/doctors?populate=*`)
